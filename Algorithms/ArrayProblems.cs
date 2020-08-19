76e082a7162589399da222b30560d449b7ef55db
ricardo <alucard_invidia@hotmail.com>
2020-08-19 16:14:41 +0800

    update

--- Algorithms/ArrayProblems.cs
+++ Algorithms/ArrayProblems.cs
@@ -766,7 +766,7 @@ namespace Algorithms
         #region 80. Remove Duplicates from Sorted Array II
         //Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
         //Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
-        //  Given nums = [1,1,1,2,2,3],
+        //  Given nums = [1,1,1,1,1,1,2,2,3],
         // Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
         // It doesn't matter what you leave beyond the returned length.
         public int RemoveDuplicates(int[] nums)
@@ -779,10 +779,10 @@ namespace Algorithms
                 int j = i + 2;
                 if (nums[i] == nums[j] || pre == nums[j])
                 {
+                    pre = nums[j];
                     nums[j] = int.MaxValue;
                     count--;
                 }
-                pre = nums[i];
             }
             for (int i = 0; i < count; i++)
             {
