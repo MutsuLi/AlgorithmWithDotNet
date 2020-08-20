368889c91e417baaff6dcc464889b91580afe403
ricardo <alucard_invidia@hotmail.com>
2020-08-20 14:34:22 +0800

    219 update

--- Algorithms/ArrayProblems.cs
+++ Algorithms/ArrayProblems.cs
@@ -797,7 +797,56 @@ namespace Algorithms
             }
             return count;
         }
+        #endregion
+        #region 219. Contains Duplicate II
 
+        // Given an array of integers and an integer k, 
+        // find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+        // Input: nums = [1,2,3,1,2,3], k = 2
+        // Output: false
+        // Input: nums = [1,2,3,1], k = 3
+        // Output: true
+        // Input: nums = [1,0,1,1], k = 1
+        // Output: true
+
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (numbers.ContainsKey(nums[i]))
+                {
+                    if (i - numbers[nums[i]] <= k)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        numbers[nums[i]] = i;
+                    }
+
+                }
+                else
+                {
+                    numbers.Add(nums[i], i);
+                }
+            }
+            return false;
+        }
+        #endregion
+
+        #region 220. Contains Duplicate III
+
+        // Given an array of integers, 
+        //find out whether there are two distinct indices i and j in the array 
+        //such that the absolute difference between nums[i] and nums[j] is at most t 
+        //and the absolute difference between i and j is at most k.
+        // Input: nums = [1,5,9,1,5,9], k = 2, t = 3
+        // Output: false
+        public bool ContainsNearbyAlmostDuplicate(int[] nums, int k, int t)
+        {
+            return false;
+        }
         #endregion
     }
 }
