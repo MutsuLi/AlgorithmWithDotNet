cf83f058ef3e2a49cfe46d1dedede5b0bd343854
ricardo <alucard_invidia@hotmail.com>
2020-10-27 16:57:05 +0800

    update

--- C++/chapter IV.cpp
+++ C++/chapter IV.cpp
@@ -8,7 +8,15 @@ using namespace std;
 */
 int main()
 {
-
+    string s1 = "a string", *p = &s1;
+    auto n = s1.size(); // run the size member of the string s1
+    n = (*p).size();    // run size on the object to which p points
+    n = p->size();      // equivalent to (*p).size()
+    unsigned num = 2;
+    num <<= 2;
+    cout << num << endl;
+    num = num + ~num;
+    cout << num << endl;
     system("pause");
     return 0;
 }
